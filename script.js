27dc632a20a78effeeb8fce0191292b9bea2009b
smolBean() <58024226+earlmae@users.noreply.github.com>
2024-08-17 12:22:05 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function swapImage() {
+    var image = document.getElementById('image');
+    image.src = 'rabbit2.png';
+}
+function resetImage() {
+    var image = document.getElementById('image');
+    image.src = 'rabbit.png';
+}
